1f010d57e01c024f48fd9dbbd99aa006bc2d7510
Calvin <calvinchan87@gmail.com>
2021-04-21 15:38:17 +0000

    create HTML markup and CSS styling for past-tweet element, display time passed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,10 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  // --- our code goes here ---
+  console.log("Timeago loaded OK");
+
+  timeago.render(document.querySelectorAll('.need_to_be_rendered'));
+  // jQuery attr() Method: Display the time passed since a tweet was created in the lower-left corner of each Tweet.
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
   // --- our code goes here ---
-  console.log("document loaded OK");
+  console.log("Document loaded OK");
 
   const textArea = document.getElementById('tweet-text');
 
   $(textArea).on('input', function() {
-    let count = $(this).val().length;
+    let count = 140 - $(this).val().length;
     // console.log(count);
     let counter = this.nextElementSibling.querySelector(".counter");
     // console.log(counter);
     $(counter).val(count);
     // console.log(counter);
-    if (count >= 141) {
+    if (count < 0) {
       $(counter).css("color", "red");
     }
-    if (count <= 140) {
+    if (count >= 0) {
       $(counter).css("color", "hotpink");
     }
   });
