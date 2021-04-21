b3e5c0469aac8a6d75f796584de9f67a1d4e519e
Calvin <calvinchan87@gmail.com>
2021-04-21 22:17:04 +0000

    tweaks folling RA call - Daniela Ornelas

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,74 +5,85 @@
  */
 
 $(document).ready(function() {
-  // --- our code goes here ---
+
   console.log("Timeago loaded OK");
 
-  timeago.render(document.querySelectorAll('.need_to_be_rendered'));
-  // jQuery attr() Method: Display the time passed since a tweet was created in the lower-left corner of each Tweet.
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
 
-// const tweetButton = document.getElementById('tweet-button');
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    for (let tweet of tweets) {
+      let $renderedTweet = createTweetElement(tweet);
+      $('.tweet-container').prepend($renderedTweet);
+      timeago.render(document.querySelectorAll('.need_to_be_rendered'));
+      // jQuery attr() Method: Display the time passed since a tweet was created in the lower-left corner of each Tweet.
+    }
+  };
 
-// $(tweetButton).on('click', function createTweetElement(obj) {
-//   console.log('Button clicked, performing ajax call...');
-//   console.log(obj);
-// });
-
-const createTweetElement = function(obj) {
-  let newObj = {
-    content: obj.content.text,
-    created: obj.created_at,
-    avatar: obj.user.avatars,
-    handle: obj.user.handle,
-    name: obj.user.name
-  }
-  const article = `
-  <article class="past-tweet">
-    <div class="tweet-header">
-      <div class="tweet-header-left">
-        <img class="far fa-user-circle" src="${newObj.avatar}"></i>
-        <div class="real-name">${newObj.name}</div>
+  const createTweetElement = function(tweet) {
+    let $tweet = `
+    <article class="past-tweet">
+      <div class="tweet-header">
+        <div class="tweet-header-left">
+          <img class="far fa-user-circle" src="${tweet.user.avatars}"></i>
+          <div class="real-name">${tweet.user.name}</div>
+        </div>
+        <div class="handle"><b>${tweet.user.handle}</b></div>
       </div>
-      <div class="handle"><b>${newObj.handle}</b></div>
-    </div>
-    <div class="tweet-article">${newObj.content}</div>
-    <div class="tweet-footer">
-      <div class="time need_to_be_rendered" datetime="${newObj.created}">Placeholder Time</div>
-      <!-- jQuery attr() Method: Display the time passed since a tweet was created in the lower-left corner of each Tweet. -->
-      <div class="icons">
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-heart"></i>
+      <div class="tweet-article">${tweet.content.text}</div>
+      <div class="tweet-footer">
+        <div class="time need_to_be_rendered" datetime="${tweet.created_at}">Placeholder Time</div>
+        <!-- jQuery attr() Method: Display the time passed since a tweet was created in the lower-left corner of each Tweet. -->
+        <div class="icons">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
       </div>
-    </div>
-  </article>
-  <br>
-  `;
-  return article;
-};
+    </article>
+    <br>
+    `;
+    return $tweet;
+  };
 
-// $.ajax('more-posts.html', { method: 'GET' })
-// .then(function (morePostsHtml) {
-//   console.log('Success: ', morePostsHtml);
-//   $button.replaceWith(morePostsHtml);
+  renderTweets(data);
 
-const $tweet = createTweetElement(tweetData);
+  // const tweetButton = document.getElementById('tweet-button');
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('.container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  // $(tweetButton).on('click', function createTweetElement(obj) {
+  //   console.log('Button clicked, performing ajax call...');
+  //   console.log(obj);
+  // });
+
+  // $.ajax('more-posts.html', { method: 'GET' })
+  // .then(function (morePostsHtml) {
+  //   console.log('Success: ', morePostsHtml);
+  //   $button.replaceWith(morePostsHtml);
 
 });
