0f6ae0369d6ab789167e5fb7971cefabe7df197f
Calvin <calvinchan87@gmail.com>
2021-04-22 03:01:33 +0000

    Fetching tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,32 +8,6 @@ $(document).ready(function() {
 
   console.log("renderTweets and Timeago loading now");
 
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
   const renderTweets = function(tweets) {
     // loops through tweets
     // calls createTweetElement for each tweet
@@ -72,18 +46,15 @@ $(document).ready(function() {
     return $tweet;
   };
 
-  renderTweets(data);
+  const loadTweets = function() {
+    console.log('Performing ajax call to loadTweets...');
+    $.ajax('/tweets', { method: 'GET' })
+      .then(function(moreTweets) {
+        console.log('loadTweets Success: ', moreTweets);
+        renderTweets(moreTweets);
+      });
+  };
 
-  // const tweetButton = document.getElementById('tweet-button');
-
-  // $(tweetButton).on('click', function createTweetElement(obj) {
-  //   console.log('Button clicked, performing ajax call...');
-  //   console.log(obj);
-  // });
-
-  // $.ajax('more-posts.html', { method: 'GET' })
-  // .then(function (morePostsHtml) {
-  //   console.log('Success: ', morePostsHtml);
-  //   $button.replaceWith(morePostsHtml);
+  loadTweets();
 
 });
